319edbddf66a521bdf972b4dc6e37c039bcf24ae
yhl221 <1727538058@qq.com>
2016-08-04 13:32:40 +0800

    finish a demo of state and props

--- js/state-props.js
+++ js/state-props.js
@@ -1,1 +1,37 @@
-// TODO
+const App=React.createClass({
+    getInitialState:function(){
+       return {
+           name:'YHL',
+           password:'123456'
+       }
+    },
+
+    editPassWord:function(){
+      this.setState({password:111111})
+    },
+
+    render:function(){
+        return <div>
+            <User name={this.state.name}/>
+            <Password password={this.state.password} onEdit={this.editPassWord}/>
+        </div>;
+    }
+});
+
+const User=React.createClass({
+    render:function(){
+    return <div>
+        <button >{this.props.name}</button>
+    </div>
+    }
+});
+
+const Password=React.createClass({
+    render:function(){
+    return  <div>
+        <button >{this.props.password}</button>
+        <button onClick={this.props.onEdit}>Edit</button>
+    </div>
+    }
+});
+ReactDOM.render(<App/>,document.getElementById('content'));
