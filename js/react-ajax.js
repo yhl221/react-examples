a130c5980efe56b17cadecf8c206640ae611591e
yhl221 <1727538058@qq.com>
2016-08-07 09:06:26 +0800

    finish reacr-ajax.js

---
+++ js/react-ajax.js
@@ -0,0 +1,34 @@
+var UserGist = React.createClass({
+    getInitialState: function() {
+        return {
+            username: '',
+            lastGistUrl: ''
+        };
+    },
+
+    componentDidMount: function() {
+        $.get(this.props.source, function(result) {
+            var lastGist = result[0];
+            if (this.isMounted()) {
+                this.setState({
+                    username: lastGist.owner.login,
+                    lastGistUrl: lastGist.html_url
+                });
+            }
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div>
+                {this.state.username}'s last gist is
+                <a href={this.state.lastGistUrl}>here</a>.
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <UserGist source="lib/react.json" />,//source是json文件相对于html文件的路径
+    document.getElementById('content')
+);
