f2a2e42ea80f603920ebffc882775caf478fed91
yhl221 <1727538058@qq.com>
2016-08-16 16:26:32 +0800

    add react example

--- js/react-ajax.js
+++ js/react-ajax.js
@@ -11,7 +11,7 @@ var UserGist = React.createClass({
             var lastGist = result[0];
             if (this.isMounted()) {
                 this.setState({
-                    username: lastGist.owner.login,
+                    us8ername: lastGist.owner.login,
                     lastGistUrl: lastGist.html_url
                 });
             }
@@ -27,6 +27,7 @@ var UserGist = React.createClass({
         );
     }
 });
+
 
 ReactDOM.render(
     <UserGist source="lib/react.json" />,//source是json文件相对于html文件的路径
